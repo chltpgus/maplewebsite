eb4df506b8b424d75f5775f8341dd88163cc7420
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-10 18:45:40 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let Question_btn1 = [];
 let Question_btn2 = [];
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-let counter = 0;
+let count = 0;
 
 for (a in array) {
     Question_screen[a-1] = document.querySelector('.Question-screen' + a);
@@ -34,7 +34,8 @@ function Question_click_1(screen) {
     Question_btn1[screen].addEventListener('click', function () {
         Question_screen[screen].style.display = 'none';
         Question_screen[screen + 1].style.display = 'block';
-        counter =0;
+        count = count + 1;
+        console.log('count');
     });
 }
 
@@ -45,8 +46,8 @@ function Question_click_2(screen) {
     });
 }
 
-    for(i=0; i<9; i++){
-        Question_click_1(i);
-        Question_click_2(i);
-        console.log('counter');
-    }
+for (i = 0; i < 9; i++) {
+    Question_click_1(i);
+    Question_click_2(i);
+
+}
