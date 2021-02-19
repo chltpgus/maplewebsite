7d25ebdeacc67457db7d3a0de3d2bd7752d27ec8
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-19 22:44:33 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ fetch('https://mapleing.herokuapp.com/api/user')
   .then(function(res) {
     /*console.log(res);*/
     user = res;
-    console.log(user[0]);
+    console.log(user[id]);
     
   });
 
