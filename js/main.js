5b9ba447cc1f8a19e0916b8c3bdd9a65941a3e1a
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-23 17:10:10 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ fetch('https://mapleing.herokuapp.com/api/user')
   .then(function(res) {
     /*console.log(res);*/
     user = res;
-    console.log(user.num);
+    console.log(user);
     user_text.innerHTML = "현재 테스트한 사용자는 "+user.num+"명 입니다.";
     
   });
